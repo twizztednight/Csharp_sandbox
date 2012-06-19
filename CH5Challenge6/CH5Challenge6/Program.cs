9ea6fb56b929f0e82bea7c43fb861a7ac38afde9
twizztednight <twizztedbz81@yahoo.com>
2012-06-19 00:05:41 -0700

    Inverted Triangle

---
+++ CH5Challenge6/CH5Challenge6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CH5Challenge6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            int myNum = int.Parse(Console.ReadLine());
+
+            for (int row = myNum; row >= 1; row--)
+            {
+                for (int col = row; col >= 1; col--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
